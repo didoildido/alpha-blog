e9e2e4224f49bf687e7813177dbe2cc81a6c323c
DDS <didoildido@outlook.com>
2023-06-26 23:05:45 +0200

    create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,18 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+    
+    end
+
+    def create
+        #render plain: params[:article] #use this for render output
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article
+        #render plain: @article.inspect
+        @article.save
+        #redirect_to article_path(@article)
+        redirect_to @article
+    end
 end
