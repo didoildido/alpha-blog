c2225eff474577de40636a976dbba1166d1b7c98
DDS <didoildido@outlook.com>
2023-07-17 23:39:20 +0200

    link pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,11 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
-
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         #render plain: params[:article] #use this for render output
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -25,5 +29,21 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])    # find the article
+        @article.destroy                        # destroy the article
+        redirect_to articles_path               # redirect to the article list
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles
+  #if want choose the option use option in the code below
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
