93f64271528773047d3c748cee9486b2a0f83de7
DDS <didoildido@outlook.com>
2023-07-20 22:36:00 +0200

    make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
-   
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show
-        @article = Article.find(params[:id])
+        
     end
 
     def index
@@ -13,12 +13,12 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+    
     end
     
     def create
         #render plain: params[:article] #use this for render output
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         #render plain: @article
         #render plain: @article.inspect
         #redirect_to article_path(@article)
@@ -32,8 +32,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully"
             redirect_to @article
         else
@@ -42,8 +41,19 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])    # find the article
         @article.destroy                        # destroy the article
         redirect_to articles_path               # redirect to the article list
     end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])    # find the article
+    end
+  
+    
+    def article_params
+        params.require(:article).permit(:title, :description)  
+    end
+
 end
